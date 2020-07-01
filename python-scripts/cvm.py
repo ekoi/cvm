4cc3d92cdc06a5fb9edf13d00648708f8fdb92fa
ekoi <eko.indarto@dans.knaw.nl>
2020-07-01 06:10:54 +0200

    intermediate commit: adding json cvmm part

--- python-scripts/cvm.py
+++ python-scripts/cvm.py
@@ -2,8 +2,11 @@ from flask import Flask, request, Response
 import requests
 import os
 import json
+import urllib3, io
 
 app = Flask(__name__)
+
+http = urllib3.PoolManager()
 
 @app.route("/v1")
 def cv():
@@ -39,6 +42,13 @@ def cvlist():
 @app.route("/", methods=['GET', 'POST'])
 def cvmanager():
     keyword = request.args.get("q")
+    if keyword == 'cvmm':
+        c = request.args.get("code")
+        json_data = create_json_cvmm(c)
+        response = Response(json_data, mimetype='application/json')
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
     vocabulary = ''
     if request.args.get("voc"):
         vocabulary = request.args.get("voc")
@@ -54,6 +64,49 @@ def cvmanager():
     response = Response(json.dumps(data), mimetype='application/json')
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
+def create_json_cvmm(code):
+    r = http.request('GET', "https://raw.githubusercontent.com/ekoi/speeltuin/master/resources/CMM_Custom_MetadataBlock.tsv")
+    d = r.data.decode('utf-8')
+    s = io.StringIO(d)
+
+    cvmm_json_result = {}
+    cv_json = []
+    start_cvm = False
+
+    for line in s:
+        if start_cvm:
+            # print(line)
+            abc = line.split('\t')
+            o_obj = {}
+            print(abc[3])
+
+            if code is None:
+                o_obj['url'] = {'type': 'uri', 'value': abc[3]}
+                o_obj['code'] = {'type': 'literal', 'value': abc[2]}
+                o_obj['prefLabel'] = {'type': 'literal', 'value': abc[2]}
+                o_obj['languagePrefLabel'] = {'type': 'literal', 'value': abc[2]}
+                o_obj['language'] = {'type': 'literal', 'value': 'en'}
+
+                cv_json.append(o_obj)
+
+            elif code in abc[3]:
+
+                o_obj['url'] = {'type': 'uri', 'value': abc[3]}
+                o_obj['code'] = {'type': 'literal', 'value': abc[2]}
+                o_obj['prefLabel'] = {'type': 'literal', 'value': abc[2]}
+                o_obj['languagePrefLabel'] = {'type': 'literal', 'value': abc[2]}
+                o_obj['language'] = {'type': 'literal', 'value': 'en'}
+
+                cv_json.append(o_obj)
+
+        if line.startswith('#controlledVocabulary'):
+            start_cvm = True
+
+    cvmm_json_result['controlledVocabulary'] = cv_json
+    json_data = json.dumps(cvmm_json_result)
+
+    return json_data
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=9266)
