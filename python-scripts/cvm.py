214b4012ac4e3bb45536c7e442ddfef44209316b
ekoi <eko.indarto@dans.knaw.nl>
2020-07-06 20:17:16 +0200

    intermediate commit: add cvmm for datasetfield

--- python-scripts/cvm.py
+++ python-scripts/cvm.py
@@ -42,26 +42,38 @@ def cvlist():
 @app.route("/", methods=['GET', 'POST'])
 def cvmanager():
     keyword = request.args.get("q")
-    if keyword == 'cvmm':
-        c = request.args.get("code")
+    c = request.args.get("code")
+    if keyword == 'cvmm-dataverse':
+        json_data = create_json_cvm_dataverse(c)
+        response = Response(json_data, mimetype='application/json')
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+    elif keyword == 'cvmm-git':
         json_data = create_json_cvmm(c)
         response = Response(json_data, mimetype='application/json')
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
 
-    vocabulary = ''
-    if request.args.get("voc"):
-        vocabulary = request.args.get("voc")
-    else:
-        vocabulary = "AnalysisUnit"
-        vocabulary = "TimeMethod"
+    elif keyword == 'CESSDA':
+        vocabulary = ''
+        if request.args.get("voc"):
+            vocabulary = request.args.get("voc")
+        else:
+            vocabulary = "AnalysisUnit"
+            vocabulary = "TimeMethod"
 
-    apienvurl = "v1/suggest/Vocabulary/%s/version/1.0/language/en/limit/10/query/%s" % (vocabulary, keyword)
-    #apiurl = "%s/%s" % (os.environ["APIHOST"], apienvurl)
-    apiurl = "%s/%s" % ("http://vocabularies.cessda.eu", apienvurl)
-    data = json.loads(requests.get(apiurl, verify=False).text)
-    data['vocab'] = vocabulary
-    response = Response(json.dumps(data), mimetype='application/json')
+        apienvurl = "v1/suggest/Vocabulary/ /version/1.0/language/en/limit/10/query/%s" % (c)
+        #apiurl = "%s/%s" % (os.environ["APIHOST"], apienvurl)
+        apiurl = "%s/%s" % ("http://vocabularies.cessda.eu", apienvurl)
+        data = json.loads(requests.get(apiurl, verify=False).text)
+        data['vocab'] = vocabulary
+        response = Response(json.dumps(data), mimetype='application/json')
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+    default_json = {}
+    response = Response(default_json, mimetype='application/json')
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
@@ -79,34 +91,62 @@ def create_json_cvmm(code):
             # print(line)
             abc = line.split('\t')
             o_obj = {}
-            print(abc[3])
+            # print(abc[3])
 
             if code is None:
-                o_obj['url'] = {'type': 'uri', 'value': abc[3]}
-                o_obj['code'] = {'type': 'literal', 'value': abc[2]}
-                o_obj['prefLabel'] = {'type': 'literal', 'value': abc[2]}
-                o_obj['languagePrefLabel'] = {'type': 'literal', 'value': abc[2]}
-                o_obj['language'] = {'type': 'literal', 'value': 'en'}
-
-                cv_json.append(o_obj)
+                cv_json.append(create_cmm_element(abc[3],abc[2],abc[2],abc[2]))
 
             elif code in abc[3]:
+                cv_json.append(create_cmm_element(abc[3],abc[2],abc[2],abc[2]))
 
-                o_obj['url'] = {'type': 'uri', 'value': abc[3]}
-                o_obj['code'] = {'type': 'literal', 'value': abc[2]}
-                o_obj['prefLabel'] = {'type': 'literal', 'value': abc[2]}
-                o_obj['languagePrefLabel'] = {'type': 'literal', 'value': abc[2]}
-                o_obj['language'] = {'type': 'literal', 'value': 'en'}
-
-                cv_json.append(o_obj)
 
         if line.startswith('#controlledVocabulary'):
             start_cvm = True
 
-    cvmm_json_result['controlledVocabulary'] = cv_json
+    cvmm_json_result['listOfCodes'] = cv_json
     json_data = json.dumps(cvmm_json_result)
 
     return json_data
+
+def create_json_cvm_dataverse(code):
+    r = http.request('GET', "https://raw.githubusercontent.com/ekoi/speeltuin/master/resources/CMM_Custom_MetadataBlock.tsv")
+    d = r.data.decode('utf-8')
+    s = io.StringIO(d)
+
+    cvmm_json_result = {}
+    cv_json = []
+    start_datasetfield = False
+
+    for line in s:
+        if line.startswith('#controlledVocabulary'):
+            start_datasetfield = False
+        if start_datasetfield:
+            # print(line)
+            abc = line.split('\t')
+            o_obj = {}
+
+            if code is None:
+                cv_json.append(create_cmm_element(abc[16],abc[2],abc[2],abc[2]))
+
+            elif code in abc[16]:
+                cv_json.append(create_cmm_element(abc[16],abc[2],abc[2],abc[2]))
+
+        if line.startswith('#datasetField'):
+            start_datasetfield = True
+
+    cvmm_json_result['listOfCodes'] = cv_json
+    json_data = json.dumps(cvmm_json_result)
+
+    return json_data
+
+def create_cmm_element(u, c, pl, lpl):
+    o_obj = {}
+    o_obj['url'] = {'type': 'uri', 'value': u}
+    o_obj['code'] = {'type': 'literal', 'value': c}
+    o_obj['prefLabel'] = {'type': 'literal', 'value': pl}
+    o_obj['languagePrefLabel'] = {'type': 'literal', 'value': lpl}
+    o_obj['language'] = {'type': 'literal', 'value': 'en'}
+    return o_obj
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=9266)
