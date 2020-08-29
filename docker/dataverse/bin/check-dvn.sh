cc7447e2fff1608be270ac1c6751c81617538df2
ekoi <eko.indarto@dans.knaw.nl>
2020-08-29 21:13:10 +0200

    add logging

--- docker/dataverse/bin/check-dvn.sh
+++ docker/dataverse/bin/check-dvn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "HELLO INDARTO" >> /tmp/status.log
+echo "Checking the Dataverse running status" >> /tmp/status.log
 until curl -sS -f "http://dataverse:8080/robots.txt" -m 2 2>&1 > /dev/null;
     do echo ">>>>>>>> Waiting for Dataverse...." >> /tmp/status.log; echo "---- Dataverse is not ready...." >> /tmp/status.log; sleep 5; done;
     echo "Dataverse is running...!" >> /tmp/status.log;
